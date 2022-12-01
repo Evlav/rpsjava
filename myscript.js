cf0a2a646815b4547a9a81a6b9e9281ca0e4bc3f
evlaV <varotpava@gmail.com>
2022-12-01 16:51:43 +0700

    Added UI functionalities

--- myscript.js
+++ myscript.js
@@ -1,17 +1,17 @@
 function getComputerChoice(){
     let output;
     let rng = Math.floor(Math.random() * 3);
-    if (rng <= 1){
+    if (rng < 1){
         output = "rock";
     }
-    else if (rng <= 2 && rng > 1){
+    else if (rng < 2 && rng >= 1){
         output = "paper";
     }
-    else if ( rng > 2){
+    else if (rng => 2){
         output = "scissor";
     }
     
-    console.log(output);
+    console.log(rng);
     return(output)
 }
 
@@ -21,35 +21,80 @@ if (playerSelection == "rock" && computerSelection == "rock"){
     return "Draw!";
 }
 else if (playerSelection == "rock" && computerSelection == "scissor"){
+    playerwins += 1;
     return "You Win!";
 }
 else if (playerSelection == "rock" && computerSelection == "paper"){
+    cpuwins += 1;
     return "You Lose!";
 }
 else if (playerSelection == "scissor" && computerSelection == "scissor"){
     return "Draw!";
 }
 else if (playerSelection == "scissor" && computerSelection == "rock"){
+    cpuwins += 1;
     return "You Lose!";
 }
 else if (playerSelection == "scissor" && computerSelection == "paper"){
+    playerwins += 1;
     return "You Win!";
 }
 else if (playerSelection == "paper" && computerSelection == "paper"){
     return "Draw!";
 }
 else if (playerSelection == "paper" && computerSelection == "scissor"){
+    cpuwins += 1;
     return "You Lose!";
 }
 else if (playerSelection == "paper" && computerSelection == "rock"){
+    playerwins += 1;
     return "You Win!";
 }
 }
 
-//main
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-
-for (let i = 0; i < 5; i++){
-    console.log(playRound(playerSelection, computerSelection));
+function displayresults(playerSelection){
+    let computerSelection = getComputerChoice();
+    results.textContent = playRound(playerSelection, computerSelection);
+    tally.textContent = "You " + playerwins + " - " + cpuwins + " CPU";
+    container.appendChild(results);
+    container.appendChild(tally);
 }
+
+//main
+let computerSelection;
+let playerSelection;
+const btn1 = document.querySelector('#btn1');
+const btn2 = document.querySelector('#btn2');
+const btn3 = document.querySelector('#btn3');
+
+btn1.addEventListener('click', () => {
+    playerSelection = 'rock';
+    displayresults(playerSelection);
+    
+});
+btn2.addEventListener('click', () => {
+    playerSelection = 'paper';
+    displayresults(playerSelection);
+});
+btn3.addEventListener('click', () => {
+    playerSelection = 'scissor';
+    displayresults(playerSelection);
+});
+
+
+
+
+//create display
+
+const container = document.querySelector('#container');
+const results = document.createElement('div');
+const tally = document.createElement('div');
+let playerwins = 0;
+let cpuwins = 0;
+
+
+
+//console.log(playRound(playerSelection, computerSelection));
+// for (let i = 0; i < 5; i++){
+    
+// }
